64b4a4cb9e6c7bf97ac0256d8010a703a50a8360
Mark Gill <mg@macbook-pro.attlocal.net>
2017-05-02 09:45:59 -0400

    lab data to controller

--- pages/lab/controller.js
+++ pages/lab/controller.js
@@ -1,6 +1,37 @@
 'use strict';
 
 angular.module('lab', ['ui.router'])
-	.controller('LabCtrl', [function() {
+	.controller('LabCtrl', ["$scope", function($scope) {
+
+		$scope.labs = [
+			{
+				'title': "r.cade",
+				'href': "https://gitmgill.github.io/rcade/#!/",
+				'summary': "r.cade is an angular app that allows users to transform list style subreddits into beautiful galleries. It's a work in progress but a lot of fun to use.",
+				'purpose': "Practicing AngularJs and consuming JSON with $http, and making something cool with Reddit in mind.",
+				'features': [
+					"Search for any subreddit",
+					"NSFW Filter"
+				],
+				'roadmap': [
+					"Native video handling",
+					"Native GIF handling",
+					"Update styling/branding",
+					"Add UI.router for states"
+				],
+				'tldr': [
+					"Lab",
+					"Reddit",
+					"API",
+					"JSON",
+					"Angular",
+					"HTML/CSS",
+					"JavaScript/jQuery",
+					"Opensource",
+					"GIT"
+				],
+				'img': "rcade4.jpg"
+			}
+		];
 
 	}]);
